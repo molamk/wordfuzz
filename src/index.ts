a4fb69b5fee0f58eb57b4cf923d487ba9f5df5e5
molamk <molamk@protonmail.com>
2019-03-08 06:36:22 -0500

    Revert to named export

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ interface IMuseResposne {
   tags: string[];
 }
 
-type MuseAnswer = IMuseResposne[];
+export declare type MuseAnswer = IMuseResposne[];
 
 const DEFAULT_PROMISE = axios.get(BASE_URL).then<MuseAnswer>(res => res.data);
 
@@ -50,4 +50,4 @@ export class Muse extends Deferred<MuseAnswer> {
   }
 }
 
-export const fuzz = Muse.init;
+export const fuzz = () => Muse.init();
